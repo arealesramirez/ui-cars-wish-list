a7e8b59019fdbdfe6c2d8b6d03de456fd1793d97
Andres Reales <arealesramirez8606@gmail.com>
2021-01-24 11:46:12 -0600

    Adding Bulma and Bulma react components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import "./App.css";
+import "./App.sass";
+
+import Container from 'react-bulma-components/lib/components/container';
+import Section from 'react-bulma-components/lib/components/section';
 
 import CarList from "./components/car-list";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <CarList />
+    <div>
+      <header className="app-header">
       </header>
+      <Section>
+        <Container>
+        <CarList />
+        </Container>
+      </Section>
     </div>
   );
 }
--- src/components/car-list.js
+++ src/components/car-list.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Heading from 'react-bulma-components/lib/components/heading';
+
 import { getCars } from "../api/car.api";
 
 export default class ListCars extends React.Component {
@@ -21,11 +23,14 @@ export default class ListCars extends React.Component {
 
   render() {
     return (
-      <ul>
-        {this.state.cars.map((car) => (
-          <li key="{car.id}">{car.model}</li>
-        ))}
-      </ul>
+      <div>
+      <Heading size={5} renderAs="p">My Car Wish List</Heading>
+        <ul>
+          {this.state.cars.map((car) => (
+            <li key="{car.id}">{car.model}</li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
